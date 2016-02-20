22a91e5f27de8050525ff52884c55989cfdf9aff
Dan Nemeth <dnemeth@pivotal.io>
2016-02-20 11:15:57 -0500

    Updated package to use cloudnativego/cfmgo.  Changed limitDefault constant.

--- const.go
+++ const.go
@@ -1,8 +1,16 @@
 package cfmgo
 
-import "errors"
+import (
+	"errors"
+	"net/url"
+
+	"github.com/cloudnativego/cfmgo/params"
+)
 
 var (
 	//ErrInvalidID -- error for invalid id
 	ErrInvalidID = errors.New("value is not a properly formatted hex string")
+
+	//ParamsUnfiltered returns a default set of parameters
+	ParamsUnfiltered = params.Extract(url.Values{})
 )
